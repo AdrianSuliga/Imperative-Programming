6a3458ae790f87ef9393d0199fc497af82a5f061
Adrian Suliga <adrian.suliga@solarboat.agh.edu.pl>
2024-03-16 19:03:15 +0100

    Started working on war exercise

--- Lab3/war_template.c
+++ Lab3/war_template.c
@@ -1,20 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 #define NUMBER_OF_CARDS 52
 #define NUMBER_OF_COLORS 4
 
 int random_from_interval(int a, int b) {
+	if (a > b)
+		return INT_MIN;
+	if (b - a > RAND_MAX)
+		return INT_MAX;
+	if (a == b)
+		return a;
+	else
+		return rand() % (b - a + 1) + a;
 }
+
 void shuffle(int t[], int size) {
+	if (size >= 0)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			t[i] = i;
+		}
+		for (int i = 0; i < size - 1; i++)
+		{
+			int k = rand_from_interval(i, size - 1);
+			swap(t, i, k);
+		}
+	}
 }
+void swap(int array[], int i, int k)
+{
+	int buffor = array[k];
+	array[k] = array[i];
+	array[i] = buffor;
+}
+
 void game(int n, int player_A[], int player_B[], int max_conflicts, int simplified_mode) {
+	int player_A_deck_size = NUMBER_OF_CARDS / 2, player_B_deck_size = 52 - player_A_deck_size;
+	int confilct_count = 0;
+
+	while (confilct_count < max_conflicts && player_A_deck_size > 0 && player_B_deck_size > 0)
+	{
+		
+	}
 }
-void first_takes_cards(int number_of_cards_on_table, int player_1[], int *pout_1, int *p_cards_1,
-		const int player_2[], int *pout_2, int *p_cards_2, int size) {
+void first_takes_cards(int number_of_cards_on_table, int player_1[], int *pout_1, int *p_cards_1, const int player_2[], int *pout_2, int *p_cards_2, int size) {
+
 }
-void both_take_cards(int player_1[], int *pout_1, const int *p_cards_1,
-		int player_2[], int *pout_2, const int *p_cards_2, int size) {
+void both_take_cards(int player_1[], int *pout_1, const int *p_cards_1, int player_2[], int *pout_2, const int *p_cards_2, int size) {
+
 }
 
 int main(void) {
