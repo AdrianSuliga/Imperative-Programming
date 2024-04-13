9b3faa7498b156be260340eead25ebddbc20b926
Adrian Suliga <adriansuliga81@gmail.com>
2024-04-13 16:55:24 +0200

    Matrix exs - solved

--- Lab6/mat_template.c
+++ Lab6/mat_template.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 #include <math.h>
 
 #define SIZE 40
@@ -36,16 +38,46 @@ void print_mat(double A[][SIZE], int m, int n) {
 // 1. Calculate matrix product, AB = A X B
 // A[m][p], B[p][n], AB[m][n]
 void mat_product(double A[][SIZE], double B[][SIZE], double AB[][SIZE], int m, int p, int n) {
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			double num_to_write = 0.0;
+			for (int k = 0; k < p; k++)
+			{
+				num_to_write += A[i][k] * B[k][j];
+			}
+			AB[i][j] = num_to_write;
+		}
+	}
 }
-
 
 // 2. Matrix triangulation and determinant calculation - simplified version
 // (no rows' swaps). If A[i][i] == 0, function returns NAN.
 // Function may change A matrix elements.
 double gauss_simplified(double A[][SIZE], int n) {
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (A[i][i] == 0)
+			return NAN;
+		for (int j = i + 1; j < n; j++)
+		{
+			double multiplier = -A[j][i] / A[i][i];
+			for (int k = i; k < n; k++)
+			{
+				A[j][k] += A[i][k] * multiplier;
+			}
+		}
+	}
+	double determinant = 1.0;
+	for (int i = 0; i < n; i++)
+		determinant *= A[i][i];
+	
+	return determinant; 
 }
 
 void backward_substitution_index(double A[][SIZE], const int indices[], double x[], int n) {
+
 }
 
 // 3. Matrix triangulation, determinant calculation, and Ax = b solving - extended version
@@ -53,13 +85,162 @@ void backward_substitution_index(double A[][SIZE], const int indices[], double x
 // swapping the rows use index vector - do not copy entire rows.)
 // If max A[i][i] < eps, function returns 0.
 // If det != 0 && b != NULL && x != NULL then vector x should contain solution of Ax = b.
-
+void swap_rows(double A[][SIZE], int r1, int r2, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		double buffor = A[r1][i];
+		A[r1][i] = A[r2][i];
+		A[r2][i] = buffor;
+	}
+}
+void insert_max(double A[][SIZE], int n, int begin, int *det_m)
+{
+	int max_val = INT_MIN;
+	int ind = 0;
+	for (int i = begin; i < n; i++)
+	{
+		if (max_val < A[i][begin])
+		{
+			max_val = A[i][begin];
+			ind = i;
+		}
+	}
+	if (ind != begin)
+	{
+		swap_rows(A, ind, begin, n);
+		(*det_m) *= -1;
+	}
+}
+void sortIndeces(double A[][SIZE], int indeces[], int n, int index, int *swaps) {
+	int temp;
+	for (int i = 0; i < n - index; i++) {
+		for (int j = index + 1; j < n - i; j++) {
+			if (fabs(A[indeces[j - 1]][index]) < fabs(A[indeces[j]][index])) {
+				temp = indeces[j];
+				indeces[j] = indeces[j - 1];
+				indeces[j - 1] = temp;
+				(*swaps)++;
+			}
+		}
+	}
+}
 double gauss(double A[][SIZE], const double b[], double x[], const int n, const double eps) {
+	int indeces[SIZE], swaps = 0;
+	double results[SIZE];
+	for (int i = 0; i < n; i++) {
+		indeces[i] = i;
+
+		if (b != NULL && x != NULL) {
+			results[i] = b[i];
+		}
+	}
+
+	if (b == NULL) {
+		x = NULL;
+	}
+
+	double c;
+	for (int i = 0; i < n; i++) {
+		sortIndeces(A, indeces, n, i, &swaps);
+
+		if (fabs(A[indeces[i]][i]) < eps) return 0;
+
+		for (int j = i + 1; j < n; j++) {
+			c = A[indeces[j]][i] / A[indeces[i]][i];
+
+			for (int k = i; k < n; k++) {
+				A[indeces[j]][k] -= A[indeces[i]][k] * c;
+			}
+
+			if (b != NULL) {
+				results[indeces[j]] -= results[indeces[i]] * c;
+			}
+		}
+	}
+
+	double det = 1;
+	for (int i = 0; i < n; i++) {
+		det *= A[indeces[i]][i];
+	}
+
+	if (b != NULL) {
+		for (int i = n - 1; i >= 0; i--) {
+			results[indeces[i]] /= A[indeces[i]][i];
+			A[indeces[i]][i] = 1;
+
+			for (int j = i - 1; j >= 0; j--) {
+				results[indeces[j]] -= results[indeces[i]] * A[indeces[j]][i];
+				A[indeces[j]][i] = 0;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		x[i] = results[indeces[i]];
+	}
+
+	return det * (swaps % 2 ? -1 : 1);
 }
 
 // 4. Returns the determinant; B contains the inverse of A (if det(A) != 0)
 // If max A[i][i] < eps, function returns 0.
 double matrix_inv(double A[][SIZE], double B[][SIZE], int n, double eps) {
+	int indeces[SIZE], swaps = 0;
+	for (int i = 0; i < n; i++) {
+		indeces[i] = i;
+	}
+
+	double result[SIZE][SIZE];
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			result[i][j] = (i == j) ? 1 : 0;
+		}
+	}
+
+	double c;
+	for (int i = 0; i < n; i++) {
+		sortIndeces(A, indeces, n, i, &swaps);
+
+		if (fabs(A[indeces[i]][i]) < eps) return 0;
+
+		for (int j = i + 1; j < n; j++) {
+			c = A[indeces[j]][i] / A[indeces[i]][i];
+
+			for (int k = 0; k < n; k++) {
+				A[indeces[j]][k] -= A[indeces[i]][k] * c;
+				result[indeces[j]][k] -= result[indeces[i]][k] * c;
+			}
+		}
+	}
+
+	double det = 1;
+	for (int i = 0; i < n; i++) {
+		det *= A[indeces[i]][i];
+	}
+
+	for (int i = n - 1; i >= 0; i--) {
+		for (int j = 0; j < n; j++) {
+			result[indeces[i]][j] /= A[indeces[i]][i];
+		}
+		A[indeces[i]][i] = 1;
+
+		for (int j = i - 1; j >= 0; j--) {
+			for (int k = 0; k < n; k++) {
+				result[indeces[j]][k] -= result[indeces[i]][k] * A[indeces[j]][i];
+			}
+
+			A[indeces[j]][i] = 0;
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			B[i][j] = result[indeces[i]][j];
+		}
+	}
+
+	return det * (swaps % 2 ? -1 : 1);
 }
 
 int main(void) {
@@ -87,7 +268,7 @@ int main(void) {
 			break;
 		case 3:
 			scanf("%d", &n);
-			read_mat(A,n, n);
+			read_mat(A, n, n);
 			read_vector(b, n);
 			det = gauss(A, b, x, n, eps);
 			printf("%.4f\n", det);
